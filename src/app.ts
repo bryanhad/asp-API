07e7c791ce044b2a21bf5700fcaf2b9c2f97093e
Bryan Hadinata <bryanhadinata76@gmail.com>
2023-12-23 22:09:09 +0700

    chore: add boilerplate

---
+++ src/app.ts
@@ -0,0 +1,4 @@
+import express from 'express'
+const app = express()
+
+app.get('/', (req, res) => res.status(200).json({message: 'Wellcome to ASP API'}))
---
+++ src/env.ts
@@ -0,0 +1,7 @@
+import { cleanEnv, port } from 'envalid'
+
+const env = cleanEnv(process.env, {
+    PORT: port()
+})
+
+export default env
